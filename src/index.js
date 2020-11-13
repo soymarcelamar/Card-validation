dc246b5f958b90ce52b3cad65af56973bdddf2e3
DayanaM <dmmartinez269@misena.edu.co>
2020-11-13 17:00:13 -0500

    Animation CSS for main screen

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-import validator from './validator.js';
-
+import validator from "./validator.js";
 console.log(validator);
+
+const intro = document.getElementById("introView");
+const footer = document.getElementById("footer");
+
+const introViewChange = (event) => {
+  console.log(event);
+};
